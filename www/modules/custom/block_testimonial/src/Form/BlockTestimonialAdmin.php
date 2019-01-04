ede359d51d91fc7ffb5d6b6571009ddd7b216a77
Martin Wessel <mwessel@herzing.edu>
2019-01-04 10:22:44 -0600

    Add fields to filter testimonials

--- www/modules/custom/block_testimonial/src/Form/BlockTestimonialAdmin.php
+++ www/modules/custom/block_testimonial/src/Form/BlockTestimonialAdmin.php
@@ -39,11 +39,11 @@ class BlockTestimonialAdmin extends ConfigFormBase {
     $form[ 'access_fields' ] = array(
       '#type'        => 'details',
       '#title'       => t( 'API Access' ),
-      '#description' => '<p>' . t( 'Determine how Client API will access the Forms API protected endpoints' ) . '</p>',
+      '#description' => '<p>' . t( 'Determine how the block will access the Testimonial API endpoints' ) . '</p>',
       '#group'       => 'vertical_tabs',
     );
 
-      $form[ 'access_fields' ][ 'path_fields' ][ 'site_url' ] = array(
+      $form[ 'access_fields' ][ 'site_url' ] = array(
         '#type'          => 'textfield',
         '#title'         => t( 'Testimonial API site URL' ),
         '#description'   => t( 'Enter the URL of the site hosting the Testimonial API' ),
@@ -53,7 +53,7 @@ class BlockTestimonialAdmin extends ConfigFormBase {
         '#required'      => TRUE,
       );
 
-      $form[ 'access_fields' ][ 'path_fields' ][ 'site_path' ] = array(
+      $form[ 'access_fields' ][ 'site_path' ] = array(
         '#type'          => 'textfield',
         '#title'         => t( 'Testimonial API site path' ),
         '#description'   => t( 'The path to the Testimonial endpoint root' ),
@@ -61,6 +61,53 @@ class BlockTestimonialAdmin extends ConfigFormBase {
         '#size'          => 200,
         '#maxlength'     => 200,
         '#required'      => TRUE,
+      );
+
+    $form[ 'filter_fields' ] = array(
+      '#type'        => 'details',
+      '#title'       => t( 'Filter Fields' ),
+      '#description' => '<p>' . t( 'Map node fields to campus filters' ) . '</p>',
+      '#group'       => 'vertical_tabs',
+    );
+
+      $form[ 'filter_fields' ][ 'campus_filter' ] = array(
+        '#type'          => 'textfield',
+        '#title'         => t( 'Campus filter' ),
+        '#description'   => t( 'The machine name of the field that will be used to filter testimonials by campus' ),
+        '#default_value' => $config->get( 'campus_filter') ?? "",
+        '#size'          => 64,
+        '#maxlength'     => 200,
+        '#required'      => false,
+      );
+
+      $form[ 'filter_fields' ][ 'campus_filter' ] = array(
+        '#type'          => 'textfield',
+        '#title'         => t( 'Campus filter' ),
+        '#description'   => t( 'The machine name of the field that will be used to filter testimonials by campus' ),
+        '#default_value' => $config->get( 'campus_filter') ?? "",
+        '#size'          => 64,
+        '#maxlength'     => 200,
+        '#required'      => false,
+      );
+
+      $form[ 'filter_fields' ][ 'area_filter' ] = array(
+        '#type'          => 'textfield',
+        '#title'         => t( 'Area filter' ),
+        '#description'   => t( 'The machine name of the field that will be used to filter testimonials by area' ),
+        '#default_value' => $config->get( 'area_filter') ?? "",
+        '#size'          => 64,
+        '#maxlength'     => 200,
+        '#required'      => false,
+      );
+
+      $form[ 'filter_fields' ][ 'program_filter' ] = array(
+        '#type'          => 'textfield',
+        '#title'         => t( 'Program filter' ),
+        '#description'   => t( 'The machine name of the field that will be used to filter testimonials by program or degree' ),
+        '#default_value' => $config->get( 'program_filter') ?? "",
+        '#size'          => 64,
+        '#maxlength'     => 200,
+        '#required'      => false,
       );
 
     $form[ 'admin_fields' ] = array(
@@ -98,7 +145,12 @@ class BlockTestimonialAdmin extends ConfigFormBase {
     $trim_mask = " \t\n\r\0\x0B\\/";
 
     $this->config('block_testimonial.blocktestimonialadmin')
-      ->set('endpoint_path', $form_state->getValue('endpoint_path'))
+      ->set('site_url', trim( $form_state->getValue('site_url'), $trim_mask ) )
+      ->set('site_path', trim( $form_state->getValue('site_path'), $trim_mask ) )
+      ->set('log_calls', trim( $form_state->getValue('log_calls'), $trim_mask ) )
+      ->set('campus_filter', trim( $form_state->getValue('campus_filter'), $trim_mask ) )
+      ->set('area_filter', trim( $form_state->getValue('area_filter'), $trim_mask ) )
+      ->set('program_filter', trim( $form_state->getValue('program_filter'), $trim_mask ) )
       ->save();
   }
 
